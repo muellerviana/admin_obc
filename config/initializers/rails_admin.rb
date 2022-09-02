3a68fd34ae4199448141bb0b571df70ec21e9de4
muellerviana <muellerviana@gmail.com>
2022-09-02 19:03:42 -0300

    config pdf generator

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,26 +1,18 @@
 RailsAdmin.config do |config|
 
-
-  config.main_app_name = ["Comercial Admin", ""]
-
-
-  config.navigation_static_links = {
-    'OneBitCode' => 'http://onebitcode.com'
-  }
-
-  config.navigation_static_label = "Other Links"
-
+  require Rails.root.join('lib', 'rails_admin', 'rails_admin_pdf.rb')
+  RailsAdmin::Config::Actions.register(RailsAdmin::Config::Actions::Pdf)
 
   ### Popular gems integration
 
-  # == Devise ==
+  ## == Devise ==
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
   config.current_user_method(&:current_user)
 
-  # == CancanCan ==
-  config.authorize_with :cancancan
+  ## == Cancan ==
+  config.authorize_with :cancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
@@ -32,111 +24,137 @@ RailsAdmin.config do |config|
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  # config.show_gravatar true
 
-  config.model Sale do 
+  config.navigation_static_links = {
+    'OneBitCode' => 'http://onebitcode.com'
+  }
+  config.navigation_static_label = "Lins Úteis"
 
+  config.main_app_name = ["Representantes Comerciais", ""]
+
+  config.model Sale do
     navigation_icon 'fa fa-money'
-    create do 
-      field :client
-      field :sale_date
-      field :user_id
-      field :discount
-      field :notes 
-      field :product_quantities 
-      field :comission 
-
-      field :user_id, :hidden do 
-        default_value do 
-          bindings[:view]._current_user.id
-        end
-      end
-    end
- 
-    edit do 
-      field :client
-      field :sale_date
-      field :user_id
-      field :discount
-      field :notes 
-      field :product_quantities 
-      field :comission 
-
-      field :user_id, :hidden do 
-        default_value do 
-          bindings[:view]._current_user.id
-        end
-      end
-    end
-  end
-
-  config.model Client do 
-    create do 
-      field :name 
-      field :company_name
-      field :document 
-      field :email 
-      field :phone 
-      field :notes 
-      field :status 
-      field :address
-      field :user_id, :hidden do 
-        default_value do 
-          bindings[:view]._current_user.id
-        end
-      end
-    end 
-
-    edit do 
-      field :name 
-      field :company_name 
-      field :document 
-      field :email 
-      field :phone 
-      field :notes 
-      field :status 
-      field :address
-      field :user_id, :hidden do 
-        default_value do
-          bindings[:view]._current_user.id 
-        end
-      end      
-    end
-
-    list do 
-      field :name 
-      field :company_name 
-      field :document 
-      field :email 
-      field :phone 
-      field :notes 
-      field :status 
-      field :address   
-    end
-  end
-
-
-  config.model ProductQuantity do 
-    visible false 
-  end
-
-  config.model Address do 
-    visible false 
-  end
-
-  config.model ProductQuantity do 
-    edit do 
-      field :product
-      field :quantity 
+    create do
+      field  :client
+      field  :sale_date
+      field  :discount
+      field  :notes
+      field  :product_quantities
 
       field :user_id, :hidden do
         default_value do
-          bindings[:view]._current_user.id 
+          bindings[:view]._current_user.id
         end
       end
     end
-  end  
- 
+
+    edit do
+      field  :client
+      field  :sale_date
+      field  :discount
+      field  :notes
+      field  :product_quantities
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+  end
+
+  config.model Client do
+    create do
+      field  :name
+      field  :company_name
+      field  :document
+      field  :email
+      field  :phone
+      field  :notes
+      field  :status
+      field  :address
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+
+    edit do
+      field  :name
+      field  :company_name
+      field  :document
+      field  :email
+      field  :phone
+      field  :notes
+      field  :status
+      field  :address
+
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+
+    list do
+      field  :name
+      field  :company_name
+      field  :document
+      field  :email
+      field  :phone
+      field  :notes
+      field  :status
+      field  :address
+
+    end
+  end
+
+
+  config.model Discount do
+    parent Product
+  end
+
+  config.model Sale do
+    parent User
+    weight -2
+  end
+
+  config.model Comission do
+    parent User
+    weight -1
+  end
+
+  config.model Client do
+    parent User
+  end
+
+  config.model ProductQuantity do
+    visible false
+  end
+
+  config.model Address do
+    visible false
+  end
+
+
+  config.model ProductQuantity do
+    edit do
+      field :product
+      field :quantity
+
+      field :user_id, :hidden do
+        default_value do
+          bindings[:view]._current_user.id
+        end
+      end
+    end
+  end
+
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
@@ -147,6 +165,9 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
+    pdf do
+      only User
+    end
 
     ## With an audit adapter, you can add:
     # history_index
